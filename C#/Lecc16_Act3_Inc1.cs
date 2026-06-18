843f7e18274a6c494b16e6dec872ce82e59c4737
25ERA01-ErickRamirez <eramirezdb25@gmail.com>
2026-06-17 21:25:20 -0600

    Add files via upload

---
+++ C#/Lecc16_Act3_Inc1.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int indice = 0;
+        int comparador = 0;
+        bool existeRepetido = false;
+        int[] numerosEnteros = new int[16];
+
+        existeRepetido = false;
+
+        for (indice = 1; indice <= 15; indice++)
+        {
+            Console.Write("Ingrese el número " + indice + ": ");
+            numerosEnteros[indice] = int.Parse(Console.ReadLine());
+        }
+
+        for (indice = 1; indice <= 14; indice++)
+        {
+            for (comparador = indice + 1; comparador <= 15; comparador++)
+            {
+                if (numerosEnteros[indice] == numerosEnteros[comparador])
+                {
+                    existeRepetido = true;
+                }
+            }
+        }
+
+        if (existeRepetido)
+        {
+            Console.WriteLine("Sí existe al menos un número repetido.");
+        }
+        else
+        {
+            Console.WriteLine("No existe ningún número repetido.");
+        }
+
+        Console.ReadKey();
+    }
+}
